86ae235de3f100754ceb3b6d3cf9ce9f0713d0eb
kunsanglee <gosmdochee2@gmail.com>
2024-03-25 15:14:02 +0900

    feat: PieceType 각 기물의 점수 반환하는 기능 구현

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -1,11 +1,21 @@
 package chess.domain.piece;
 
 public enum PieceType {
-    PAWN,
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    KING,
-    QUEEN,
-    NONE;
+    KING(0),
+    PAWN(1),
+    KNIGHT(2.5),
+    BISHOP(3),
+    ROOK(5),
+    QUEEN(9),
+    NONE(0);
+
+    private final double score;
+
+    PieceType(double score) {
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
 }
