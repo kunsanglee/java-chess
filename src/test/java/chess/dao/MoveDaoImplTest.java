7c46d99e1968709091c4cef8dfada0522c1afdd7
kunsanglee <gosmdochee2@gmail.com>
2024-03-30 00:03:41 +0900

    test: 주석처리된 테스트 제거

--- src/test/java/chess/dao/MoveDaoImplTest.java
+++ src/test/java/chess/dao/MoveDaoImplTest.java
@@ -2,8 +2,6 @@ package chess.dao;
 
 import chess.domain.position.Position;
 import chess.dto.MoveRequest;
-import chess.dto.MoveResponse;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,16 +20,5 @@ class MoveDaoImplTest {
         Position target2 = Position.from("b4");
 
         moveDaoImpl.save(MoveRequest.of(source2, target2, 1L));
-    }
-
-    @DisplayName("기물의 움직임을 전부 불러올 수 있다")
-    @Test
-    void findAllMoveDao() {
-        List<MoveResponse> moves = moveDaoImpl.findAll(1L);
-
-//        assertThat(moves).isEqualTo(List.of(
-//                new MoveResponse(1L, "a2", "a4", 1L),
-//                new MoveResponse(2L, "b2", "b4", 1L)
-//        ));
     }
 }
