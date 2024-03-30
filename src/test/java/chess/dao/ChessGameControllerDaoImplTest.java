28a586e11a1cbc40f64f0063ca6d8e513758ad43
kunsanglee <gosmdochee2@gmail.com>
2024-03-30 20:56:45 +0900

    test: DB 테스트코드 Fake 객체를 사용한 테스트로 변경

--- src/test/java/chess/dao/ChessGameControllerDaoImplTest.java
+++ src/test/java/chess/dao/ChessGameControllerDaoImplTest.java
@@ -1,34 +1,72 @@
 package chess.dao;
 
+import static chess.domain.GameStatus.FINISHED;
+import static chess.domain.GameStatus.PLAYING;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import chess.domain.GameStatus;
 import chess.dto.ChessGameRequest;
 import chess.dto.ChessGameResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ChessGameControllerDaoImplTest {
-    private final ChessGameDaoImpl chessGameDaoImpl = new ChessGameDaoImpl();
+    private ChessGameDao chessGameDao = new FakeChessGameDaoImpl();
 
     @DisplayName("진행중인 체스 게임을 저장할 수 있다.")
     @Test
     void savePlayingChessGame() {
         ChessGameRequest chessGameRequest = new ChessGameRequest(GameStatus.PLAYING.name());
-        chessGameDaoImpl.save(chessGameRequest);
-    }
+        Long gameId = chessGameDao.save(chessGameRequest);
 
-    @DisplayName("종료된 체스 게임을 저장할 수 있다.")
-    @Test
-    void saveFinishedChessGame() {
-        ChessGameRequest chessGameRequest = new ChessGameRequest(GameStatus.FINISHED.name());
-        chessGameDaoImpl.save(chessGameRequest);
+        assertThat(gameId).isEqualTo(1L);
     }
 
     @DisplayName("진행중인 마지막 게임을 불러올 수 있다")
     @Test
     void findRecentGame() {
-        Optional<ChessGameResponse> recentPlayableGame = chessGameDaoImpl.findRecentPlayableGame();
+        chessGameDao.save(new ChessGameRequest(GameStatus.PLAYING.name()));
+        Optional<ChessGameResponse> recentPlayableGame = chessGameDao.findRecentPlayableGame();
 
-        recentPlayableGame.ifPresent(System.out::println);
+        assertThat(recentPlayableGame.isPresent()).isTrue();
+        assertThat(recentPlayableGame.get().gameStatus()).isEqualTo(GameStatus.PLAYING.name());
+    }
+
+    @DisplayName("킹이 죽으면 이전 게임들의 상태를 모두 Finished 상태로 업데이트한다")
+    @Test
+    void givenKingIsDeadWhenUpdateGameFinishedThenPreviousGamesFinished() {
+        chessGameDao.updateGameFinished();
+
+        assertThat(chessGameDao.findRecentPlayableGame().isEmpty()).isTrue();
+    }
+
+    static class FakeChessGameDaoImpl implements ChessGameDao {
+        private List<ChessGameRequest> requests = new ArrayList<>();
+
+        @Override
+        public Long save(ChessGameRequest chessGameRequest) {
+            requests.add(chessGameRequest);
+            return (long) requests.size();
+        }
+
+        @Override
+        public Optional<ChessGameResponse> findRecentPlayableGame() {
+            return requests.stream()
+                    .filter(request -> PLAYING.name().equals(request.gameStatus()))
+                    .map(request -> new ChessGameResponse((long) requests.indexOf(request) + 1, request.gameStatus()))
+                    .reduce((first, second) -> second);
+        }
+
+        @Override
+        public void updateGameFinished() {
+            List<ChessGameRequest> updatedRequests = requests.stream()
+                    .map(request -> new ChessGameRequest(FINISHED.name()))
+                    .collect(Collectors.toList());
+            this.requests = updatedRequests;
+        }
     }
 }
--- src/test/java/chess/dao/MoveDaoImplTest.java
+++ src/test/java/chess/dao/MoveDaoImplTest.java
@@ -2,23 +2,67 @@ package chess.dao;
 
 import chess.domain.position.Position;
 import chess.dto.MoveRequest;
+import chess.dto.MoveResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MoveDaoImplTest {
-    private final MoveDaoImpl moveDaoImpl = new MoveDaoImpl();
+    private final MoveDao moveDao = new FakeMoveDaoImpl();
 
-    @DisplayName("moveDao를 저장할 수 있다.")
+    @DisplayName("새로운 이동 정보를 저장할 수 있다")
     @Test
-    void saveMoveDao() {
+    void saveMove() {
         Position source = Position.from("a2");
         Position target = Position.from("a4");
-
-        moveDaoImpl.save(new MoveRequest(source.getValue(), target.getValue(), 1L));
+        moveDao.save(new MoveRequest(source.getValue(), target.getValue(), 1L));
 
         Position source2 = Position.from("b2");
         Position target2 = Position.from("b4");
+        moveDao.save(new MoveRequest(source2.getValue(), target2.getValue(), 1L));
 
-        moveDaoImpl.save(new MoveRequest(source2.getValue(), target2.getValue(), 1L));
+        List<MoveResponse> moves = moveDao.findAll(1L);
+        Assertions.assertThat(moves).hasSize(2);
+    }
+
+    @DisplayName("특정 체스 게임의 모든 이동 정보를 찾을 수 있다")
+    @Test
+    void findAllMovesByGameId() {
+        MoveRequest moveRequest1 = new MoveRequest("e2", "e4", 1L);
+        MoveRequest moveRequest2 = new MoveRequest("e7", "e5", 1L);
+        moveDao.save(moveRequest1);
+        moveDao.save(moveRequest2);
+
+        List<MoveResponse> moves = moveDao.findAll(1L);
+
+        Assertions.assertThat(moves)
+                .hasSize(2)
+                .extracting(MoveResponse::source)
+                .containsExactly("e2", "e7");
+
+        Assertions.assertThat(moves)
+                .extracting(MoveResponse::target)
+                .containsExactly("e4", "e5");
+    }
+
+    static class FakeMoveDaoImpl implements MoveDao {
+        private final List<MoveResponse> moves = new ArrayList<>();
+        private long currentId = 1;
+
+        @Override
+        public void save(MoveRequest moveRequest) {
+            moves.add(new MoveResponse(currentId++, moveRequest.source(), moveRequest.target(),
+                    moveRequest.chessGameId()));
+        }
+
+        @Override
+        public List<MoveResponse> findAll(Long id) {
+            return moves.stream()
+                    .filter(move -> move.chessGameId().equals(id))
+                    .collect(Collectors.toList());
+        }
     }
 }
